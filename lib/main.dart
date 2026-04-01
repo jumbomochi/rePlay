6714c56b98ce5f614a7d9a6c33b2fe4e20bfd3b7
Huiliang Lui <huiliang@gmail.com>
2026-04-01 21:49:18 +0800

    chore: add anthropic_sdk_dart and flutter_dotenv dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env').catchError((_) {});
   runApp(
     const ProviderScope(
       child: RePlayApp(),
